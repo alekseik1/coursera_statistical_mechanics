4e2af48aa06b2449a37776c98d10071cd670d5e7
alekseik1 <1alekseik1@gmail.com>
2020-07-05 13:25:31 +0300

    w3: update markov chain

--- w3/hw/my_markov_disks_box.py
+++ w3/hw/my_markov_disks_box.py
@@ -1,6 +1,7 @@
 import random
 import math
 import pylab
+import os
 
 
 def show_conf(L, sigma, title, fname):
@@ -29,14 +30,41 @@ def dist(x, y):
     return math.sqrt(d_x**2 + d_y**2)
 
 
+def load_cache(filename, sigma, N):
+    if os.path.isfile(filename):
+        f = open(filename, 'r')
+        L = []
+        for line in f:
+            a, b = line.split()
+            L.append([float(a), float(b)])
+        f.close()
+        print('starting from file', filename)
+    else:
+        L = []
+        N_sqrt = int(N**0.5)
+        delxy = max(1/N, sigma)
+        two_delxy = 2*delxy
+        for k in range(N):
+            L = [[delxy + i * two_delxy, delxy + j * two_delxy] for i in range(N_sqrt) for j in range(N_sqrt)]
+        print('starting from a new random configuration')
+    return L
+
+
+def save_cache(filename):
+    with open(filename, 'w') as f:
+        for a in L:
+            f.write(str(a[0]) + ' ' + str(a[1]) + '\n')
+
+
 if __name__ == '__main__':
-    N = 4
-    L = [[0.25, 0.25], [0.75, 0.25], [0.25, 0.75], [0.75, 0.75]]
+    N = 64
+    eta = 0.72
+    n_steps = 10000
     # eta = N * (pi * sigma ** 2)
-    eta = 0.5
     sigma = math.sqrt(eta/(N*math.pi))
-    delta = 0.1
-    n_steps = int(1e4)
+    delta = 0.5*sigma
+    filename = 'disk_configuration_N%i_eta%.2f.txt' % (N, eta)
+    L = load_cache(filename, sigma, N)
     for steps in range(n_steps):
         current_particle = random.choice(L)
         current_particle_next = [current_particle[0] + random.uniform(-delta, delta),
@@ -47,4 +75,5 @@ if __name__ == '__main__':
         if min_dist >= 2*sigma:
             current_particle[:] = current_particle_next
             L = [[p[0] % 1.0, p[1] % 1.0] for p in L]
+    save_cache(filename)
     show_conf(L, sigma, 'hello', '1.png')
